aa854fcfd422550654ec33348b93343bd3308798
diegobobasso <88977281+diegobobasso@users.noreply.github.com>
2025-10-16 19:09:49 -0300

    actualizado a html

--- src/error/errorHandlers.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    notFoundPage: (req, res) => {
-      // carga la vista de pagina de error 404 pagina no encontrada
-      res.status(404).render('Error', {title: 'Error 404', error: 'Error 404: pagina no encontrada...'});
-    }
-  }
